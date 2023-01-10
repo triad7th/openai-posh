8e58767ab058ca82656e3ce8a5d60bf07b2a928c
Kevin Lee <wonseok.lee80@gmail.com>
2023-01-09 20:13:51 -0800

    psm1 update - token from file

--- OpenAI.psm1
+++ OpenAI.psm1
@@ -1,18 +1,24 @@
 Write-Host "OpenAI Powershell Module" -ForegroundColor Black -BackgroundColor Yellow -NoNewLine;
 Write-Host -BackgroundColor Black
 #region Global Variables
-    Write-Host "Loading: `$OpenAI"
-    $OpenAI = [PSCustomObject]@{
-        Cfg = (Get-Content -Path "$PSScriptRoot\OpenAI.json" | ConvertFrom-JSON)
-    }
+Write-Host "Loading: `$OpenAI"
+$OpenAI = [PSCustomObject]@{
+  Cfg = (Get-Content -Path "$PSScriptRoot\OpenAI.json" | ConvertFrom-JSON)
+}
 #endregion
 foreach ($directory in @('Public')) {    
-    Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" -Recurse -File | ForEach-Object { 
-        Write-Host "Loading: $directory\$($_.Name)" -ForegroundColor White -BackgroundColor Black -NoNewLine;
-        Write-Host -BackgroundColor Black
-        . $_.FullName 
-    }
+  Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" -Recurse -File | ForEach-Object { 
+    Write-Host "Loading: $directory\$($_.Name)" -ForegroundColor White -BackgroundColor Black -NoNewLine;
+    Write-Host -BackgroundColor Black
+    . $_.FullName 
+  }
 }
 
-Get-OpenAIToken | Set-OpenAIToken
+if (Test-Path '.\api.token') {
+  Get-Content -Path '.\api.token' | ConvertTo-SecureString | Set-OpenAIToken
+}
+else {
+  Get-OpenAIToken | Set-OpenAIToken
+}
+
 Export-ModuleMember -Variable OpenAI
